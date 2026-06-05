c8e23e62f436bb724b8600b9fc2d7ccaff6abda8
kazumenda <McWicio@gmail.com>
2026-06-05 14:11:28 +0200

    Add project files.

---
+++ RolayVilla_API/Controllers/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RolayVilla_API.Models;
+
+namespace RolayVilla_API.Controllers.Data
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Villa> Villa { get; set; }
+    }
+}
---
+++ RolayVilla_API/Controllers/VillaControler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RolayVilla_API.Controllers
+{
+    [Route("api/villas")]
+    [ApiController]
+    public class VillaControler : ControllerBase
+    {
+        [HttpGet]
+        public string GetVilllas()
+        {
+            
+            return "This is the list of villas";
+        }
+
+        [HttpGet("{id:int}")]
+        public string GetVilllasById(int id)
+        {
+
+            return "This is the list of villas by ID" + id;
+        }
+    }
+}
